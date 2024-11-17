75913c8200f1fb0f62a14a658ff175edf2fb7206
Francisco <dev.fran00@gmail.com>
2024-11-16 20:18:47 -0600

    Correccion de errores

--- src/main/java/com/fran/game_service/controller/GameApi.java
+++ src/main/java/com/fran/game_service/controller/GameApi.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fran.game_service.commons.constans.ApiPathVariables;
@@ -15,14 +16,14 @@ import com.fran.game_service.commons.entities.Game;
 @RequestMapping(ApiPathVariables.V1_ROUTE + ApiPathVariables.GAME_ROUTE)
 public interface GameApi {
     @PostMapping
-    public ResponseEntity<Game> saveGame(@RequestBody Game game);
+    public ResponseEntity<Game> saveGame(@RequestHeader("userIdRequest") String userId, @RequestBody Game game);
 
     @GetMapping("/{gameId}")
-    public ResponseEntity<Game> getGame(@PathVariable Long gameId);
+    public ResponseEntity<Game> getGame(@RequestHeader("userIdRequest") String userId, @PathVariable Long gameId);
 
     @PutMapping("/{id}")
-    public ResponseEntity<Game> putMethodName(@PathVariable Long gameId, @RequestBody Game game);
+    public ResponseEntity<Game> putMethodName(@RequestHeader("userIdRequest") String userId, @PathVariable Long gameId, @RequestBody Game game);
 
     @DeleteMapping("/{gameId}")
-    public ResponseEntity<Object> deleteMethodName(@PathVariable Long gameId);
+    public ResponseEntity<Object> deleteMethodName(@RequestHeader("userIdRequest") String userId, @PathVariable Long gameId);
 }
--- src/main/java/com/fran/game_service/controller/impl/GameController.java
+++ src/main/java/com/fran/game_service/controller/impl/GameController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import com.fran.game_service.commons.entities.Game;
 import com.fran.game_service.controller.GameApi;
@@ -20,29 +21,31 @@ public class GameController implements GameApi {
     }
     
     @Override
-    public ResponseEntity<Game> saveGame(@RequestBody Game game) { 
+    public ResponseEntity<Game> saveGame(@RequestHeader("userIdRequest") String userId, @RequestBody Game game) {
+        System.out.println("USER ID" + userId);
         Game cratedGame = this.gameService.saveGame(game);
         return ResponseEntity.ok(cratedGame);
     
     }
 
     @Override
-    public ResponseEntity<Game> getGame(@PathVariable Long gameId) { 
+    public ResponseEntity<Game> getGame(@RequestHeader("userIdRequest") String userId, @PathVariable Long gameId) {
+        System.out.println("USER ID: " + userId);       
         Game gameObtained = this.gameService.getGame(gameId);
         return ResponseEntity.ok(gameObtained);
     }
 
     @Override
-    public ResponseEntity<Game> putMethodName(@PathVariable Long gameId, @RequestBody Game game) {       
+    public ResponseEntity<Game> putMethodName(@RequestHeader("userIdRequest") String userId, @PathVariable Long gameId, @RequestBody Game game) {       
         Game updatedGgame = this.gameService.putGame(gameId, game);
         return ResponseEntity.ok(updatedGgame);
     }
 
     @Override
-    public ResponseEntity<Object> deleteMethodName(@PathVariable Long gameId) {       
+    public ResponseEntity<Object> deleteMethodName(@RequestHeader("userIdRequest") String userId, @PathVariable Long gameId) {       
         this.gameService.deleteGame(gameId);
    
         return ResponseEntity.ok(HttpStatus.OK);
-    }
+    } 
 
-}
+} 
